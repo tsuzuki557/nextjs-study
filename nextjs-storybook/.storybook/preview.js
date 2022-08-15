e6722b22f524da130a7392b9d72130baaeb16499
tsuzuki557 <goodin557@gmail.com>
2022-08-15 09:50:55 +0900

    storybook init

---
+++ nextjs-storybook/.storybook/preview.js
@@ -0,0 +1,23 @@
+import * as NextImage from 'next/image'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+// next/imageの差し替え
+// const OriginalNextImage = NextImage.default;
+
+// Object.defineProperty(NextImage, 'default', {
+//   configurable: true,
+//   value: (props) => typeof props.src === 'string' ? (
+//     <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+//   ) : (
+//     <OriginalNextImage {...props} unoptimized />
+//   ),
+// })
