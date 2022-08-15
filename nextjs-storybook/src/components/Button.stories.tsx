96277a0d7dcb2b0f480d0f7436a0b07fbc609f3d
tsuzuki557 <goodin557@gmail.com>
2022-08-15 16:43:19 +0900

    CSF3.0での書き方

--- nextjs-storybook/src/components/Button.stories.tsx
+++ nextjs-storybook/src/components/Button.stories.tsx
@@ -1,25 +1,36 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Button from "./Button";
 
+// ref. CSF3.0 https://storybook.js.org/blog/component-story-format/
 export default {
-  title: "SAMPLE/Button",
+  title: "SAMPLE/Button", // CSF3.0ではtitl自動付与されるため、任意
   component: Button,
   argTypes: {
     backgroundColor: { control: "color" },
   },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+// CSF2.0
 
-export const Basic1 = Template.bind({});
-Basic1.args = {
-  primary: true,
-  label: "ボタンAA",
+//const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+// export const Basic1 = Template.bind({});
+// Basic1.args = {
+//   primary: true,
+//   label: "ボタンAA",
+// };
+
+// export const Basic2 = Template.bind({});
+// Basic2.args = {
+//   primary: false,
+//   label: "ボタンB",
+//   size: "LL",
+// };
+
+// CSF3.0
+export const Basic1 = {
+  args: { primary: true, label: "ボタンAA" },
 };
-
-export const Basic2 = Template.bind({});
-Basic2.args = {
-  primary: false,
-  label: "ボタンB",
-  size: "LL",
+export const Basic2 = {
+  args: { primary: false, label: "ボタンB", size: "LL" },
 };
