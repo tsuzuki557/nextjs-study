b9a1f38877b329e1b7b964b20f66d546a922961b
tsuzuki557 <goodin557@gmail.com>
2022-08-15 15:54:58 +0900

    gazou

--- nextjs-storybook/.storybook/preview.js
+++ nextjs-storybook/.storybook/preview.js
@@ -11,14 +11,13 @@ export const parameters = {
   },
 }
 
-// next/imageの差し替え
-// const OriginalNextImage = NextImage.default;
-
-// Object.defineProperty(NextImage, 'default', {
-//   configurable: true,
-//   value: (props) => typeof props.src === 'string' ? (
-//     <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
-//   ) : (
-//     <OriginalNextImage {...props} unoptimized />
-//   ),
-// })
+// Storybookでは最適化されていないnext/imageコンポーネントを使用するため
+const OriginalNextImage = NextImage.default;
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => typeof props.src === 'string' ? (
+    <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+  ) : (
+    <OriginalNextImage {...props} unoptimized />
+  ),
+})
