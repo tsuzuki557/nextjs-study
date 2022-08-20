2f0576ddb6ffd14ca1d2e3a1d562d69ad3cbdbe8
tsuzuki557 <goodin557@gmail.com>
2022-08-20 11:59:43 +0900

    整理

---
+++ nextjs-webgl/src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>実験</title>
+      </Head>
+
+      <main>
+        <h1>webglの実験</h1>
+      </main>
+
+      <footer>フッター</footer>
+    </div>
+  );
+};
+
+export default Home;
